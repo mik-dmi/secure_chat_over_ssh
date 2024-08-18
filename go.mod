00def8813006df2abebb24b81bbfb00572b7e849
mike_dev <122227240+mik-dmi@users.noreply.github.com>
2024-08-18 19:38:37 +0100

    Developing the chat  connections

--- go.mod
+++ go.mod
@@ -3,9 +3,13 @@ module secure_chat_over_ssh
 go 1.22.6
 
 require (
+	github.com/gliderlabs/ssh v0.3.7
+	golang.org/x/crypto v0.26.0
+	golang.org/x/term v0.23.0
+)
+
+require (
 	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
-	github.com/gliderlabs/ssh v0.3.7 // indirect
-	golang.org/x/crypto v0.26.0 // indirect
+	github.com/teris-io/shortid v0.0.0-20220617161101-71ec9f2aa569 // indirect
 	golang.org/x/sys v0.23.0 // indirect
-	golang.org/x/term v0.23.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,41 +1,71 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"log"
 	"os"
+	"sync"
 
 	"github.com/gliderlabs/ssh"
+	"github.com/teris-io/shortid"
 	gossh "golang.org/x/crypto/ssh"
 	"golang.org/x/term"
 )
 
 type SSHHandler struct {
 }
+type User struct {
+	session      ssh.Session
+	nameTag      string
+	connectionID string
+}
+
+var clients sync.Map
+
+func NewUser(session ssh.Session, userName string) *User {
+	randomID, err := shortid.Generate()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &User{
+		session:      session,
+		nameTag:      userName,
+		connectionID: randomID,
+	}
+}
+func (u *User) AddUserToMap() {
+	clients.Store(u.connectionID, u)
+}
+func (u *User) RemoveUserFromMap() {
+	clients.Delete(u.connectionID)
+}
 
 func (h *SSHHandler) handleSSHSession(session ssh.Session) {
-	io.WriteString(session, "Remote forwarding available...\n")
-
 	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
 	if err != nil {
 		panic(err)
 	}
 	defer term.Restore(int(os.Stdin.Fd()), oldState)
 	term := term.NewTerminal(session, "> ")
+	term.Write([]byte("Welcome to secure chat!!!\n What's your Name Tag?\n"))
+	nameTag, err := term.ReadLine()
 	term.Write([]byte("What do you want to join?\n- Chat Room (cmd: CR)\n- Chat One On One (cmd: COOO)\n"))
-
 	userChoice, err := term.ReadLine()
 	if err != nil {
 		log.Fatal(err)
 	}
 	for {
-
 		switch userChoice {
 		case "CR":
 			term.Write([]byte("Joined a chat room"))
+
 			return
 		case "COOO":
-			term.Write([]byte("Individual chat room created"))
+			//fmt.Println("Local Addr : ", session.LocalAddr().String())
+			user := NewUser(session, nameTag)
+
+			fmt.Println("SSH connection established successfully!")
+			fmt.Println("Print user info: ", user)
 			return
 		default:
 			term.Write([]byte("Wrong command, try gain:\n"))
@@ -56,6 +86,23 @@ func NewSSHHandler() *SSHHandler {
 
 func main() {
 	sshPort := ":2222"
+
+	handler := NewSSHHandler()
+	server := &ssh.Server{
+		Addr:    sshPort,
+		Handler: handler.handleSSHSession,
+		PublicKeyHandler: (func(ctx ssh.Context, key ssh.PublicKey) bool {
+			return true
+		}),
+		ServerConfigCallback: func(ctx ssh.Context) *gossh.ServerConfig {
+			cfg := &gossh.ServerConfig{
+				ServerVersion: "SSH-2.0-OpenSSH_8.9p1",
+			}
+			cfg.Ciphers = []string{"chacha20-poly1305@openssh.com"}
+			return cfg
+		},
+	}
+
 	b, err := os.ReadFile("./keys/private_key")
 	if err != nil {
 		log.Fatal(err)
@@ -65,18 +112,7 @@ func main() {
 	if err != nil {
 		log.Fatal("Failed to parse private key: ", err)
 	}
-
-	handler := NewSSHHandler()
-	server := &ssh.Server{
-		Addr:        sshPort,
-		Handler:     handler.handleSSHSession,
-		HostSigners: []ssh.Signer{privateKey},
-		PublicKeyHandler: (func(ctx ssh.Context, key ssh.PublicKey) bool {
-			return true
-		}),
-		Version: "SSH-2.0-OpenSSH_8.9p1",
-	}
-
+	//fmt.Println("Key in the main : ", privateKey)
 	server.AddHostKey(privateKey)
 	log.Printf("Starting SSH server on port %s", sshPort)
 	log.Fatal(server.ListenAndServe())
